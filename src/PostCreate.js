1ff41061a6e9e4b11f7f101a222685a294b970cd
Badri <programmingwithbadri@outlook.com>
2020-07-12 16:29:44 +0530

    Building the post submit page with api call

---
+++ src/PostCreate.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+export default () => {
+  // Returns a stateful object and a function to update it
+  // eg) setTitle() will be used to update the title object;
+  // setTitle(1) => title = 1;
+  const [title, setTitle] = useState('');
+
+  const onSubmit = async event => {
+    event.preventDefault();
+
+    await axios.post('http://localhost:4000/posts', {
+      title
+    });
+
+    setTitle('');
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Title</label>
+          <input
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+            className="form-control"
+          />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+};
